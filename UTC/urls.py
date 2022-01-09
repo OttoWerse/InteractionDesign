9c26bf44a0a2d11fc6dc7e9a89d632b08e1b731e
Yannis Grützmacher <fuer.unwichtig@web.de>
2022-01-09 16:09:19 +0100

    Tutorial 4/7 fertig Server läuft noch Weiterhin nur mit Tutorial HTMLs getestet
    index.html hat namespacing
    neue vote view

--- UTC/urls.py
+++ UTC/urls.py
@@ -4,12 +4,8 @@ from . import views
 
 app_name = 'UTC'
 urlpatterns = [
-    # ex: /UTC/
-    path('', views.index, name='index'),
-    # ex: /UTC/5/
-    path('<int:question_id>/', views.detail, name='detail'),
-    # ex: /UTC/5/results/
-    path('<int:question_id>/results/', views.results, name='results'),
-    # ex: /UTC/5/vote/
+    path('', views.IndexView.as_view(), name='index'),
+    path('<int:pk>/', views.DetailView.as_view(), name='detail'),
+    path('<int:pk>/results/', views.ResultsView.as_view(), name='results'),
     path('<int:question_id>/vote/', views.vote, name='vote'),
 ]
