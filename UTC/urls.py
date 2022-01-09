98fc9b3fb6e79c50fa9ceb21af68141c9d9e06e1
Yannis Grützmacher <fuer.unwichtig@web.de>
2022-01-09 15:07:56 +0100

    Tutorial 3/7 template directory ist da Server läuft noch Bisher nur mit Tutorial HTMLs getestet
    index.html hat namespacing
    neue vote view

--- UTC/urls.py
+++ UTC/urls.py
@@ -4,12 +4,12 @@ from . import views
 
 app_name = 'UTC'
 urlpatterns = [
-    # ex: /polls/
+    # ex: /UTC/
     path('', views.index, name='index'),
-    # ex: /polls/5/
+    # ex: /UTC/5/
     path('<int:question_id>/', views.detail, name='detail'),
-    # ex: /polls/5/results/
+    # ex: /UTC/5/results/
     path('<int:question_id>/results/', views.results, name='results'),
-    # ex: /polls/5/vote/
+    # ex: /UTC/5/vote/
     path('<int:question_id>/vote/', views.vote, name='vote'),
 ]
