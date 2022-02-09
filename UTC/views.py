546f82b691ef45e22bc787256c37d4f550d827e4
Otto Werse <otto.werse@web.de>
2022-02-09 23:55:30 +0100

    Returned to working state, base HTML site for registration form done.

--- UTC/views.py
+++ UTC/views.py
@@ -1,47 +1,16 @@
-"""
-Alte views.py ohne shortcut vom Tutorial behalten
-
-from django.http import HttpResponse
-from django.template import loader
-
-from .models import Question
-
-
-def index(request):
-    latest_question_list = Question.objects.order_by('-pub_date')[:5]
-    template = loader.get_template('UTC/index.html')
-    context = {
-        'latest_question_list': latest_question_list,
-    }
-    return HttpResponse(template.render(context, request))
-
-
-def detail(request, question_id):
-    return HttpResponse("You're looking at question %s." % question_id)
-
-
-def results(request, question_id):
-    response = "You're looking at the results of question %s."
-    return HttpResponse(response % question_id)
-
-
-def vote(request, question_id):
-    return HttpResponse("You're voting on question %s." % question_id)
-"""
-
 from django.http import HttpResponseRedirect
-from django.shortcuts import get_object_or_404, render
-from django.urls import reverse
+from django.shortcuts import render
 from django.views import generic
 
+from .forms import StudentForm
 from .models import Student
 
 
-class IndexView():
+class IndexView(generic.TemplateView):
     template_name = 'UTC/index.html'
 
 
-class IslandView():
+class IslandView(generic.TemplateView):
     template_name = 'UTC/island.html'
 
 
@@ -55,38 +24,6 @@ class ResultsView(generic.DetailView):
     template_name = 'UTC/results.html'
 
 
-class SignUpView(generic.CreateView):
-    form_class = UserCreationForm
-    success_url = reverse_lazy('login')
-    template_name = 'registration/signup.html'
-
-
-# ...
-def vote(request, question_id):
-    question = get_object_or_404(Question, pk=question_id)
-    try:
-        selected_choice = question.choice_set.get(pk=request.POST['choice'])
-    except (KeyError, Choice.DoesNotExist):
-        # Redisplay the question voting form.
-        return render(request, 'UTC/detail.html', {
-            'question': question,
-            'error_message': "You didn't select a choice.",
-        })
-    else:
-        selected_choice.votes += 1
-        selected_choice.save()
-        # Always return an HttpResponseRedirect after successfully dealing
-        # with POST data. This prevents data from being posted twice if a
-        # user hits the Back button.
-        return HttpResponseRedirect(reverse('UTC:results', args=(question.id,)))
-
-
-"""
-alte views
-def vote(request, question_id):
-    return HttpResponse("You're voting on question %s." % question_id)
-    
-def results(request, question_id):
-    response = "You're looking at the results of question %s."
-    return HttpResponse(response % question_id)
-"""
+class FormVIew(generic.FormView):
+    form_class = StudentForm
+    template_name = 'UTC/form.html'
