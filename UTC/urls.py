8005063a861b87c1a9f273eea9e379840a698994
Yannis Grützmacher <fuer.unwichtig@web.de>
2022-02-10 19:00:56 +0100

    Koks und Nutten und Surenhohn Es steht in der verfickten Datenbank Heil Busse!

--- UTC/urls.py
+++ UTC/urls.py
@@ -8,4 +8,5 @@ urlpatterns = [
     path('form/', views.FormVIew.as_view(), name='form'),
     path('island/', views.IslandView.as_view(), name='island'),
     path('<int:pk>/', views.DetailView.as_view(), name='detail'),
+    path('registered/', views.registered, name='registered'),
 ]
--- UTC/views.py
+++ UTC/views.py
@@ -27,3 +27,62 @@ class ResultsView(generic.DetailView):
 class FormVIew(generic.FormView):
     form_class = StudentForm
     template_name = 'UTC/form.html'
+    success_url = '/UTC/'
+
+
+def registered(request):
+    form = StudentForm(request.POST)
+    form.save()
+    return render(request, 'UTC/index.html', {'form': form})
+
+
+'''
+    form = StudentForm(request.POST)
+    form.save()
+    return render(request, 'UTC/index.html', {'form': form})
+    
+    print('Surenhohn')
+    name = request.POST['name']
+    vorname = request.POST['vorname']
+    strasse = request.POST['strasse']
+    hausnummer = request.POST['hausnummer']
+    plz = request.POST['plz']
+    ort = request.POST['ort']
+    land = request.POST['land']
+    geburtsdatum = request.POST['geburtsdatum']
+    telephone = request.POST['telephone']
+    mail = request.POST['mail']
+    studiengang = request.POST['studiengang']
+    nameDerHochschule = request.POST['nameDerHochschule']
+    tarif_choice = request.POST['tarif_choice']
+    anmerkung = request.POST['anmerkung']
+    koffer = request.POST['koffer']
+    zahlung_choice = request.POST['zahlung_choice']
+    agb = request.POST['agb']
+
+    s = Student(name=name, vorname=vorname, strasse=strasse, hausnummer=hausnummer, plz=plz, ort=ort,
+                land=land, geburtsdatum=geburtsdatum, telephone=telephone, mail=mail,
+                studiengang=studiengang, nameDerHochschule=nameDerHochschule, tarif_choice=tarif_choice,
+                anmerkung=anmerkung, koffer=koffer, zahlung_choice=zahlung_choice, agb=agb)
+    s.save()
+    return render(request, 'UTC/registered.html', {'name': name})
+
+
+    # if this is a POST request we need to process the form data
+    if request.method == 'POST':
+        # create a form instance and populate it with data from the request:
+        form = StudentForm(request.POST)
+        # check whether it's valid:
+        if form.is_valid():
+            form.save()
+            # process the data in form.cleaned_data as required
+            # ...
+            # redirect to a new URL:
+            return HttpResponseRedirect('/UTC/')
+
+    # if a GET (or any other method) we'll create a blank form
+    else:
+        form = StudentForm()
+
+    return render(request, 'UTC/index.html', {'form': form})
+'''
